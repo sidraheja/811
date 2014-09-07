95b1f75492893c6b8a8e25315c80fef82add2f0d
Siddarth Raheja <siddarth.raheja@gmail.com>
2014-09-07 13:56:29 -0400

    working model with basic feature

--- 811/811/ViewController.m
+++ 811/811/ViewController.m
@@ -7,8 +7,9 @@
 //
 
 #import "ViewController.h"
+#import <MessageUI/MessageUI.h>
 
-@interface ViewController ()
+@interface ViewController () <MFMessageComposeViewControllerDelegate>
 
 @end
 
@@ -24,6 +25,94 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)lockDoor:(id)sender
+{
+    NSLog(@"Lock!");
+    [self showSMS];
+    
+    
+    /*
+    //Account Details
+    NSString *TwilioSID = @"ACc98b6fe34c1db23f909d3aa43e7bbe7b";
+    NSString *TwilioAuth = @"9788f8150f8b830f0cad46604eba4b4f";
+    NSString *FromNumber = @"+17745458098";
+    NSString *SendNumber = @"+17745458098";
+    NSString *Message = @"TWhats uo guys";
+    
+    // Build request
+    NSString *urlString = [NSString stringWithFormat:@"https://%@:%@@api.twilio.com/2010-04-01/Accounts/%@/SMS/Messages", TwilioSID, TwilioAuth, TwilioSID];
+    NSURL *url = [NSURL URLWithString:urlString];
+    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
+    [request setURL:url];
+    [request setHTTPMethod:@"POST"];
+    
+    // Set up the body
+    NSString *bodyString = [NSString stringWithFormat:@"From=%@&To=%@&Body=%@", FromNumber, SendNumber,Message];
+    NSData *data = [bodyString dataUsingEncoding:NSUTF8StringEncoding];
+    [request setHTTPBody:data];
+    NSError *error;
+    NSURLResponse *response;
+    NSData *receivedData = [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&error];
+    
+    // Handle the received data
+    if (error) {
+        NSLog(@"Error: %@", error);
+    } else {
+        NSString *receivedString = [[NSString alloc]initWithData:
+                                    receivedData encoding:NSUTF8StringEncoding];
+        NSLog(@"Request sent. %@", receivedString);
+    }
+     */
+}
+
+- (IBAction)unlockDoor:(id)sender
+{
+     NSLog(@"Unlock!");
+}
+
+- (void)messageComposeViewController:(MFMessageComposeViewController *)controller didFinishWithResult:(MessageComposeResult) result
+{
+    switch (result) {
+        case MessageComposeResultCancelled:
+            break;
+            
+        case MessageComposeResultFailed:
+        {
+            UIAlertView *warningAlert = [[UIAlertView alloc] initWithTitle:@"Error" message:@"Failed to send SMS!" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+            [warningAlert show];
+            break;
+        }
+            
+        case MessageComposeResultSent:
+            break;
+            
+        default:
+            break;
+    }
+    
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (void)showSMS
+{
+
+    if(![MFMessageComposeViewController canSendText]) {
+        UIAlertView *warningAlert = [[UIAlertView alloc] initWithTitle:@"Error" message:@"Your device doesn't support SMS!" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+        [warningAlert show];
+        return;
+    }
+    
+    NSArray *recipents = @[@"7745458098"];
+    NSString *message = [NSString stringWithFormat:@"L"];
+    MFMessageComposeViewController *messageController = [[MFMessageComposeViewController alloc] init];
+    messageController.messageComposeDelegate = self;
+    [messageController setRecipients:recipents];
+    [messageController setBody:message];
+    
+    // Present message view controller on screen
+    [self presentViewController:messageController animated:YES completion:nil];
 }
 
 @end
